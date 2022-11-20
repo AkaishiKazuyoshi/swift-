28d4d4f4f62db68ebdc9498a1c2f7194ac205fc0
akaishi <kazuyoshi_akaishi@mail.toyota.co.jp>
2022-11-20 22:34:59 +0900

    Initial Commit

---
+++ akaishi20221120/akaishi20221120App.swift
@@ -0,0 +1,17 @@
+//
+//  akaishi20221120App.swift
+//  akaishi20221120
+//
+//  Created by user on 2022/11/20.
+//
+
+import SwiftUI
+
+@main
+struct akaishi20221120App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
